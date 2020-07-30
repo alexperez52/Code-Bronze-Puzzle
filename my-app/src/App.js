eaeed33974d1b97254fc7e37fbc18b20ce9c7f98
alexis <alexperez52@gmail.com>
2020-07-30 12:59:32 -0400

    first guess set, and tested component change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,27 +4,47 @@ import './App.css';
 class App extends Component {
 
   state = {
-    guess: 0
+    turn: 0,
+    possibilities: ['a', 'b', 'c', 'd', 'e', 'f'],
+    current: []
   }
 
   componentDidMount() {
-    const guess = this.takeGuess();
-    this.setState({ guess: guess });
+
   }
 
-  takeGuess() {
+  play() {
+    /*
+    Make random guess
+    Get feedback e.g (2 0)
+    Filter possible results
+    Choose the one that yields best results
+  */
+    if (this.state.turn % 2 === 0) {
+      let firstGuess = ['a', 'b', 'c'];
+      this.setState({ current: firstGuess });
+    }
+    else {
+      let firstGuess = ['c', 'b', 'a'];
+      this.setState({ current: firstGuess });
+    }
 
-    const num = Math.floor(Math.random() * 6) + 1;
-    console.log(num);
-    return num;
+    var upCount = this.state.turn + 1;
+    this.setState({ turn: upCount });
+
   }
+
 
 
   render() {
 
     return (
       <div>
-        {this.state.guess}
+
+        {this.state.current}
+        <input placeholder="correct"></input>
+        <input placeholder="correct location"></input>
+        <button onClick={() => this.play()}> submit </button>
       </div>
     );
   }
