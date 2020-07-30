51d45b030dcbf74cbefcd5a55aadad45d735cf3d
alexis <alexperez52@gmail.com>
2020-07-30 10:17:33 -0400

    Testing static function on page reload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    guess: 0
+  }
+
+  componentDidMount() {
+    const guess = this.takeGuess();
+    this.setState({ guess: guess });
+  }
+
+  takeGuess() {
+
+    const num = Math.floor(Math.random() * 6) + 1;
+    console.log(num);
+    return num;
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        {this.state.guess}
+      </div>
+    );
+  }
 }
 
 export default App;
