19ee2bdbc501bd2ddac70b276db232347a088bc4
alexis <alexperez52@gmail.com>
2020-07-30 23:39:13 -0400

    Can make guesses and update UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import BKnife from './images/bknife.png';
+import Fan from './images/fan.png';
+import Mask from './images/mask.png';
+import Revolver from './images/revolver.png';
+import Torch from './images/torchlamp.png';
+import WoodBlock from './images/woodblock.png';
 
 class App extends Component {
 
   state = {
     turn: 0,
-    possibilities: ['a', 'b', 'c', 'd', 'e', 'f'],
-    current: ['a', 'b', 'c'],
-    correctNum: 0,
-    correctPos: 0
+    possibilities: [BKnife, Fan, Mask, Revolver, Torch, WoodBlock],
+    current: [],
+
   }
 
   componentDidMount() {
 
   }
+
   componentDidUpdate() {
-    console.log(this.state.turn);
+    console.log(this.state.current);
   }
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
 
-  play() {
+  play(e) {
     /*
     Make random guess
     Get feedback e.g (2 0)
     Filter possible results
     Choose the one that yields best results
   */
-    var cn = this.state.correctNum;
-    var cp = this.state.correctPos;
-
-    this.makeGuess(cn, cp);
-
-
-
-    var upCount = this.state.turn + 1;
-    this.setState({ turn: upCount });
-
+    console.log(e);
+    if (this.state.current.length < 3 && !this.state.current.includes(e)) {
+      this.setState({ current: [...this.state.current, e] })
+    }
   }
 
-  makeGuess(cn, cp) {
-    var temp = ['a', this.state.possibilities[cn], this.state.possibilities[cp]]
-    console.log(temp);
-    this.setState({ current: temp });
+  clear() {
+    this.setState({ current: [] })
   }
 
 
@@ -53,17 +48,25 @@ class App extends Component {
 
     return (
       <div>
+        <div>
+          Tap on image to guess
+        {this.state.possibilities.map((items, index) =>
+          <img src={items}
+            key={index}
+            onClick={() => this.play(index)}></img>
+        )}
+        </div>
 
-        {this.state.current}
-        <input placeholder="correct"
-          name="correctNum"
-          id="correctNum"
-          onChange={this.onChange}></input>
-        <input placeholder="correct location"
-          name="correctPos"
-          id="correctPos"
-          onChange={this.onChange}></input>
-        <button onClick={() => this.play()}> submit </button>
+
+        <div>
+          Current guess
+        {this.state.current.map((items, index) =>
+          <img key={index} src={this.state.possibilities[items]}></img>
+        )}
+        </div>
+        <div>
+          <button onClick={() => this.clear()}>clear</button>
+        </div>
       </div>
     );
   }
